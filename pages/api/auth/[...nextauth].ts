48888c76e632709205d35c246bd57fde6be9cbd4
Craig Schwartz <craigschwartz525@gmail.com>
2022-10-04 11:46:31 -0400

    Add session callback to get user id

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,22 @@ import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
+    callbacks: {
+        async session({ session, token }) {
+            console.log('session', session);
+            console.log('token', token);
+
+            if (session?.user) {
+                session.user.id = token.sub as string;
+            }
+
+            // If user exists in the data store, pull user's data
+
+            // If user does not exist in the data store, create and persist a record for the user
+
+            return session;
+        }
+    },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
